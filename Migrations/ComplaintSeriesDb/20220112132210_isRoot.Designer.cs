832304550c4973475ad1f8ec547e1305b24c2f7b
Adrian Eduard <banila.eduard@gmail.com>
2022-02-01 14:04:00 +0200

    minor changes, updated connection string to connect to local mysql database

--- Migrations/ComplaintSeriesDb/20220112132210_isRoot.Designer.cs
+++ Migrations/ComplaintSeriesDb/20220112132210_isRoot.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApi.Entities;
 
 namespace BackArt.Migrations.ComplaintSeriesDb
 {
     [DbContext(typeof(ComplaintSeriesDbContext))]
-    partial class ComplaintSeriesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220112132210_isRoot")]
+    partial class isRoot
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/ComplaintSeriesDb/20220112132210_isRoot.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackArt.Migrations.ComplaintSeriesDb
+{
+    public partial class isRoot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isRoot",
+                table: "CodeLinkSnapshot",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isRoot",
+                table: "CodeLinkSnapshot");
+        }
+    }
+}
